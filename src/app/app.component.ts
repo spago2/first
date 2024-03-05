253e6e106900d8fd4dfa7096859e785f821aeb7c
rafaldominiak <rafal.dominiak@devspire.eu>
2024-03-05 18:02:01 +0100

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first';
+  liczbaWylosowana = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+  
+  liczbaZgadywana = 0;
+  
+  zgadnij(){
+    if(this.liczbaZgadywana===this.liczbaWylosowana){
+      alert("Super zgadłeś");
+    
+    
+    }else{alert("Nie zgadłeś");}
+  }
 }
+
